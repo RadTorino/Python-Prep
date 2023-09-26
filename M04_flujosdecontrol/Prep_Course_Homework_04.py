94dd1b270d411326e4ea830693e6ec05851b1998
Conrado Torino <121633549+RadTorino@users.noreply.github.com>
2023-09-26 20:43:59 -0300

    Add files via upload

--- M04_flujosdecontrol/Prep_Course_Homework_04.py
+++ M04_flujosdecontrol/Prep_Course_Homework_04.py
@@ -6,7 +6,13 @@
 # 1) Crear una variable que contenga un elemento del conjunto de números enteros y luego imprimir por pantalla si es mayor o menor a cero
 
 # In[4]:
-
+num = 3
+if num > 0:
+    print("Mayor a cero")
+elif num < 0:
+    print("Menor a cero")
+else:
+    print("Igual a cero")
 
 
 
@@ -14,7 +20,12 @@
 # 2) Crear dos variables y un condicional que informe si son del mismo tipo de dato
 
 # In[5]:
-
+nombre= "Rad"
+edad= 26
+if type(edad) == type(nombre):
+    print("Variables de mismo tipo")
+else:
+    print("Variables de diferente tipo")
 
 
 
@@ -22,6 +33,11 @@
 # 3) Para los valores enteros del 1 al 20, imprimir por pantalla si es par o impar
 
 # In[7]:
+for i in range(1,21):
+    if (i%2 == 0):
+        print( str(i) + "es par")
+    else:
+        print(str(i) + "es impar")
 
 
 
@@ -30,7 +46,8 @@
 # 4) En un ciclo for mostrar para los valores entre 0 y 5 el resultado de elevarlo a la potencia igual a 3
 
 # In[9]:
-
+for i in range(0, 6):
+    print(i**3)
 
 
 
@@ -38,7 +55,9 @@
 # 5) Crear una variable que contenga un número entero y realizar un ciclo for la misma cantidad de ciclos
 
 # In[10]:
-
+n= 10
+for i in range(0, n):
+    
 
 
 
@@ -46,14 +65,28 @@
 # 6) Utilizar un ciclo while para realizar el factoreo de un número guardado en una variable, sólo si la variable contiene un número entero mayor a 0
 
 # In[33]:
-
-
-
+n = 33
+if type(n) == int:
+    if n >0:
+        while n > 1:
+            a= n
+            n= a * (n-1)
+            n -=1
+        print(n)
+    else:
+        print("No mayor a cero")
+else:
+    print("número no entero")
 
 
 # 7) Crear un ciclo for dentro de un ciclo while
 
 # In[38]:
+n = 0
+while n < 7 :
+    for i in range(2):
+        print("Copa libertadores n°", i)
+        n+=1
 
 
 
@@ -70,6 +103,19 @@
 # 9) Imprimir los números primos existentes entre 0 y 30
 
 # In[54]:
+for i in range(0,31):
+    if i==0:
+        print(f"N {i} es primo")
+    j = i-1
+    while j > 1:
+        if i%j != 0:
+            j-=1
+            continue
+        else:
+            break
+    if j == 1:
+        print(f"N {i} es primo")
+
 
 
 
@@ -97,7 +143,11 @@
 # 12) Aplicando continue, armar un ciclo while que solo imprima los valores divisibles por 12, dentro del rango de números de 100 a 300
 
 # In[62]:
-
+n = 100
+while n < 301:
+    if n%12 == 0:
+        print(n)
+    n+=1
 
 
 
@@ -105,6 +155,27 @@
 # 13) Utilizar la función **input()** que permite hacer ingresos por teclado, para encontrar números primos y dar la opción al usario de buscar el siguiente
 
 # In[73]:
+i= 0
+while True:
+    if i==0:
+        print(f"N° {i} es primo")
+        if (input("Apreta 1 si querés seguir:") != "1"):
+            break
+        else:
+            i +=1
+    j = i-1
+    while j > 1:
+        if (i%j) != 0:
+            j-=1
+            continue
+        else:
+            break
+    if j <= 1:
+        print(f"N° {i} es primo")
+        if (input("Apreta 1 si querés seguir: ") != "1"):
+            print("Programa finalizado")
+            break
+    i+=1
 
 
 
@@ -113,5 +184,14 @@
 
 # In[75]:
 
-
+n=100
+flag = False
+while n < 301:
+    if n % 3 ==0:
+        if n%6 ==0:
+            print(f"{n} es divisible por 3 y múltiplo de 6")
+            flag=True
+    n+=1      
+    if flag:
+        break
 
